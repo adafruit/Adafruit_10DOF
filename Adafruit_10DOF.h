c489ff7724a2520a26bf05ef803e8c9da2b9871b
llabad39 <lucaslabadens@hotmail.fr>
2017-04-18 13:47:26 +0200

    merging library

--- Adafruit_10DOF.h
+++ Adafruit_10DOF.h
@@ -27,6 +27,8 @@
 #include <Adafruit_L3GD20_U.h>
 #include <Wire.h>
 
+
+
 /** Sensor axis */
 typedef enum
 {
@@ -48,6 +50,66 @@ class Adafruit_10DOF
     bool  fusionGetOrientation ( sensors_event_t *accel_event, sensors_event_t *mag_event, sensors_vec_t *orientation );
 
   private:
+};
+
+class Adafruit_10DOF_Kalman_Filter{
+public:
+    Adafruit_10DOF_Kalman_Filter(float _dt);
+    bool begin(sensors_event_t *accel_event,float offset_gyro[3]);
+    bool update(sensors_event_t *accel_event,sensors_event_t *gyro_event,sensors_vec_t *orientation);
+    void set_Q_angle(float _Q_angle);
+    void set_Q_velocity(float _Q_velocity);
+    void set_R(float _R);
+private:
+    //time between 2 update in seconde.
+    float dt;
+    
+    //Calibration of the gyroscope
+    float gyro_cal[3]={0,0,0};
+    
+    //Axis calulate with the accelerometer sensor
+    //------------------------------
+    float Accel_pitch = 0;
+    
+    float Accel_roll = 0;
+    
+    //Prediction Matrix
+    //-----------------------------
+    float Predicted_pitch = 0;
+    float Velocity_pitch= 0;
+    
+    float Predicted_roll = 0;
+    float Velocity_roll= 0;
+    
+    
+    //noise in the covariance matrix
+    //---------------------
+    float Q_angle = 0.1;
+    float Q_velocity = 0.1;
+    
+    //measurement noise
+    //---------------------
+    float R = 5;
+    
+    //Covariance Matrix
+    //--------------------
+    float PX00 = 0;
+    float PX01 = 0;
+    float PX10 = 0;
+    float PX11 = 0;
+    
+    float PY00 = 0;
+    float PY01 = 0;
+    float PY10 = 0;
+    float PY11 = 0;
+    
+    //Matrix of Kalman gain.
+    //---------------
+    float KX0, KX1;
+    
+    float KY0, KY1;
+    
+    void accelGetOrientation(sensors_event_t *accel_event);
 };
 
 #endif
